8434efc34bad5131e98677ff69845e5df9ef37ca
mabsboza <marcosboza@moi-solutions.com>
2019-10-14 21:44:34 -0600

    Portafolio

---
+++ style-switcher/style-switcher.js
@@ -0,0 +1,39 @@
+$(function(){
+	
+	$('head').append('<link rel="stylesheet" href="style-switcher/style-switcher.css" type="text/css" />');	
+	$('head').append('<script type="text/javascript" src="style-switcher/jquery.cookie.js"></script>');	
+				
+	// Display after some time 
+	$(".switcher .switch-h").delay("1500").fadeIn(3000);
+		
+	setTimeout(function(){ $('.switcher .switch-h').fadeOut() }, 10000);	
+
+	$('.switch').click(function() {
+		var $slidebox=$('.switcher');
+		// var $s_hide=1;
+		if($slidebox.css('left')=="-251px"){
+		  $slidebox.animate({left:0},300);
+		}
+		else{
+		  $slidebox.animate({left:-251},300);
+		  // $.cookie('switch', $s_hide);
+		}
+	});    
+	
+	
+	// Color Changer
+	// By Cookie
+	/*if($.cookie('mikiColor')!=null){
+		var color_code = $.cookie('mikiColor');
+		$('link[id="color_theme"]').attr('href', 'static/css/color/'+color_code+'.css');
+	}*/
+	// By click
+	$('.s-color a').click(function(e){
+		e.preventDefault();
+		var color_code = $(this).attr('data-code');
+		$('link[id="color_theme"]').attr('href', 'static/css/color/'+color_code+'.css');
+		$.cookie('mikiColor', color_code);
+	});
+	
+	
+});
